2358cbb928607f91a9f77b9087c95ece1ab346e4
Sarfaroz Yunusov <imSarfaroz@users.noreply.github.com>
2021-02-13 23:34:57 +0500

    project-01 uva11849 unfinished

---
+++ project-01/uva-11849/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <unordered_set>
+
+using namespace std;
+
+int main()
+{
+
+    while (true)
+    {
+        int jack;
+        int jill;
+
+        cin >> jack;
+        cin >> jill;
+
+        if (jack == 0 && jill == 0)
+        {
+            break;
+        }
+    }
+}
