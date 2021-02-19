5b0f1f8575544bdc87057ff1915e09735cf7ad20
Sarfaroz Yunusov <imSarfaroz@users.noreply.github.com>
2021-02-19 23:05:05 +0500

    lab-02 p01 hash function

--- lab-02/p01/main.cpp
+++ lab-02/p01/main.cpp
@@ -2,12 +2,15 @@
 #include <unordered_set>
 #include <string>
 #include <sstream>
+#include <iomanip>
 
 using namespace std;
 
 int main()
 {
     unordered_set<string> setOfWords;
+
+    auto hash = setOfWords.hash_function();
 
     for (string line; getline(cin, line);)
     {
@@ -21,6 +24,8 @@ int main()
         {
             string word;
             sinp >> word;
+            cout << "hash value: " << hash(word) << endl;
+            cout << "bucket's index: " << hash(word) % setOfWords.bucket_count() << endl;
             auto p = setOfWords.insert(word);
             cout << (p.second ? "YES" : "NO") << endl;
         }
@@ -28,6 +33,8 @@ int main()
         {
             string word;
             sinp >> word;
+            cout << "hash value: " << hash(word) << endl;
+            cout << "bucket's index: " << hash(word) % setOfWords.bucket_count() << endl;
             auto it = setOfWords.find(word);
             cout << (it != setOfWords.end() ? "YES" : "NO") << endl;
         }
@@ -35,16 +42,21 @@ int main()
         {
             string word;
             sinp >> word;
+            cout << "hash value: " << hash(word) << endl;
+            cout << "bucket's index: " << hash(word) % setOfWords.bucket_count() << endl;
             cout << (setOfWords.erase(word) ? "YES" : "NO") << endl;
         }
         else if (cmd == "#")
         {
-            for (size_t)
-                for (auto it = setOfWords.begin(); it != setOfWords.end(); ++it)
+            for (size_t i = 0; i < setOfWords.bucket_count(); ++i)
+            {
+                cout << setw(4) << i << ": ";
+                for (auto p = setOfWords.begin(i); p != setOfWords.end(i); ++p)
                 {
-                    cout << " " << *it;
+                    cout << " " << *p;
                 }
-            cout << endl;
+                cout << endl;
+            }
         }
     }
 }
