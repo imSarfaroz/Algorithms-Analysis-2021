d78a28a6af0ebea7eb31a35e6466d69102f8b977
Sarfaroz Yunusov <imSarfaroz@users.noreply.github.com>
2021-02-19 23:17:03 +0500

    project01 uva-12049 unfinished

---
+++ project-01/uva-12049/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <unordered_set>
+
+using namespace std;
+//Just prume the list
+
+int main()
+{
+
+    int testCase;
+    cin >> testCase;
+
+    for (int i = 0; i < testCase; ++i)
+    {
+
+        unordered_set<int, int> aMap;
+        unordered_set<int, int> bMap;
+
+        int listA;
+        int listB;
+        cin >> listA;
+        cin >> listB;
+
+        for (int j = 0; j < listA; ++j)
+        {
+            int temp;
+            cin >> temp;
+        }
+    }
