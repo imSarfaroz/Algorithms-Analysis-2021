e5b1df162944f57098d3810d7150a8d12574bd3d
Sarfaroz Yunusov <imSarfaroz@users.noreply.github.com>
2021-02-24 00:38:45 +0500

    project01 uva-11062 incomplete logic

---
+++ project-01/uva-11062/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <set>
+
+using namespace std;
+
+int main()
+{
+    set<string> words;
+    string lineOfWords;
+
+    while (getline(cin, lineOfWords))
+    {
+        // need to read words
+        // lowercase
+        // insert to set
+        // print
+
+        //* erase "-"
+    }
+}
