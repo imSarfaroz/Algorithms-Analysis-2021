e81591962d47f7473db8e7c1a157907edbf3756d
Sarfaroz Yunusov <imSarfaroz@users.noreply.github.com>
2021-02-25 19:58:49 +0500

    project01 uva-978 incomplete logic

---
+++ project-01/uva-978/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int main()
+{
+    int numOfCase;
+    cin >> numOfCase;
+
+    for (int i = 0; i < numOfCase; i++)
+    {
+        multiset<int, int> gl;
+        multiset<int, int> bl;
+
+        int bf;
+        cin >> bf;
+        int g;
+        cin >> g;
+        int b;
+        cin >> b;
+    }
+}
