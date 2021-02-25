586bffc4de1a534865b8d7972449410e8fe2b254
Sarfaroz Yunusov <imSarfaroz@users.noreply.github.com>
2021-02-25 15:37:57 +0500

    project01 uva-11136 incomplete logic

---
+++ project-01/uva-11136/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int main()
+{
+    int numOfdays;
+    cin >> numOfdays;
+
+    while (numOfdays != 0)
+    {
+        // set
+        // reading data
+        // insert in set
+        // print
+    }
+}
