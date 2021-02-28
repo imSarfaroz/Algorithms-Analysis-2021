8ca4993fcf5b5b5afee39566466494fa22b55363
Sarfaroz Yunusov <imSarfaroz@users.noreply.github.com>
2021-02-28 23:29:06 +0500

    project01 uva-10887 wrong answer

--- project-01/uva-10887/main.cpp
+++ project-01/uva-10887/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <unordered_set>
 #include <string>
+#include <vector>
 
 using namespace std;
 
@@ -8,6 +9,7 @@ int main()
 {
     int numOfCases;
     cin >> numOfCases;
+    int count = 1;
 
     for (int i = 0; i < numOfCases; i++)
     {
@@ -15,7 +17,32 @@ int main()
         int b;
         cin >> a;
         cin >> b;
-        unordered_set<string> lanA(a);
-        unordered_set<string> lanB(b);
+        vector<string> lanA(a);
+        vector<string> lanB(b);
+        getchar();
+
+        unordered_set<string> languages;
+
+        for (int i = 0; i < a; i++)
+        {
+            getline(cin, lanA[i]);
         }
+
+        for (int i = 0; i < b; i++)
+        {
+            getline(cin, lanB[i]);
+        }
+
+        cout << "Case " << count++;
+
+        for (int j = 0; j < b; ++j)
+        {
+            for (int k = 0; k < a; ++k)
+            {
+                languages.insert(lanA[k] + lanB[j]);
+            }
+        }
+
+        cout << languages.size();
+    }
 }
