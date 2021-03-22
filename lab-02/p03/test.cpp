21ed76620aef53b224ad17e389169ce952f3691f
Sarfaroz Yunusov <imSarfaroz@users.noreply.github.com>
2021-03-22 16:05:40 +0500

    lab02 p03 functions

---
+++ lab-02/p03/test.cpp
@@ -0,0 +1,19 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "../../doctest.h"
+
+#include "BStreeInt.hpp"
+
+TEST_CASE("Default constructor")
+{
+    BStreeInt s;
+
+    REQUIRE(s.size() == 0);
+}
+
+TEST_CASE("insert")
+{
+}
+
+TEST_CASE("find")
+{
+}
