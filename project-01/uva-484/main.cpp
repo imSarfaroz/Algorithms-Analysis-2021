b8bfd873a899b9db03a9658f3ef01a054748737a
Sarfaroz Yunusov <imSarfaroz@users.noreply.github.com>
2021-03-27 15:24:30 +0500

    project01 uva-484 Presentation error

---
+++ project-01/uva-484/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+int main()
+{
+
+    vector<int> order;
+    unordered_map<int, int> freq;
+
+    int input;
+
+    while (cin >> input)
+    {
+
+        if (freq[input] == 0)
+        {
+            order.emplace_back(input);
+        }
+
+        ++freq[input];
+    }
+
+    for (auto &o : order)
+    {
+        cout << o << freq[o] << endl;
+    }
+}
