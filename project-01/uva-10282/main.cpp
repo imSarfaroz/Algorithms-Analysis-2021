74230b0a394389cee6730feecaee86623a19bf41
Sarfaroz Yunusov <imSarfaroz@users.noreply.github.com>
2021-03-27 15:31:31 +0500

    project01 uva10282 unfinished

---
+++ project-01/uva-10282/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <unordered_map>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    unordered_map<string, string> dict;
+    string line;
+
+    while (getline(cin, line))
+    {
+        // need to read two words from a line
+
+        // calculate
+
+        // print
+    }
+}
