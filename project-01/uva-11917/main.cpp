3d0bccc196f832a47e3a7aa8077049eacf6fccf8
Sarfaroz Yunusov <imSarfaroz@users.noreply.github.com>
2021-03-28 18:01:52 +0500

    project01 uva-11917 incomplete logic

---
+++ project-01/uva-11917/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <unordered_map>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int numOfCases;
+    cin >> numOfCases;
+
+    for (int i = 0; i < numOfCases; i++)
+    {
+        unordered_map<string, int> sub;
+
+        int HW;
+        cin >> HW;
+
+        for (int j = 0; j < HW; j++)
+        {
+            // read subkect and time
+
+            // read a due date
+        }
+    }
+}
