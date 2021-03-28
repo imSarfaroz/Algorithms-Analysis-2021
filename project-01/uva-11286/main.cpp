aee71efabb7cea1b2168240e350ee0fae5127cea
Sarfaroz Yunusov <imSarfaroz@users.noreply.github.com>
2021-03-28 17:20:43 +0500

    project01 uva-11286 incomplete logic

---
+++ project-01/uva-11286/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    while (true)
+    {
+        int numOfCases;
+        cin >> numOfCases;
+
+        map<vector<int>, int> cmp;
+
+        for (int i = 0; i < numOfCases; i++)
+        {
+        }
+    }
+}
