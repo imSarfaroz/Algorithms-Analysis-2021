773b8ea35d56509a4e1df7616af24975ce4639ca
Sarfaroz Yunusov <imSarfaroz@users.noreply.github.com>
2021-03-28 17:29:14 +0500

    porject01 uva-11286 wrong logic

--- project-01/uva-11286/main.cpp
+++ project-01/uva-11286/main.cpp
@@ -11,10 +11,30 @@ int main()
         int numOfCases;
         cin >> numOfCases;
 
-        map<vector<int>, int> cmp;
+        map<vector<int>, int> cmb;
 
         for (int i = 0; i < numOfCases; i++)
         {
+            vector<int> v(5);
+            for (auto &x : v)
+            {
+                cin >> x;
+            }
+            ++cmb[v];
         }
+
+        int cmbFreq = 0;
+        for (auto &p : cmb)
+        {
+            cmbFreq = max(cmbFreq, p.second);
+        }
+
+        int ans = 0;
+        for (auto &p : cmb)
+        {
+            if (p.second == cmbFreq)
+                ++ans;
+        }
+        cout << cmbFreq * ans << endl;
     }
 }
