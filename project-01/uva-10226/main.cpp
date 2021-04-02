faa02f840037d986397f85116ebeca08edde6fcd
Sarfaroz Yunusov <imSarfaroz@users.noreply.github.com>
2021-04-03 00:32:48 +0500

    project01 uva-10226 incomplete

---
+++ project-01/uva-10226/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <map>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    string name;
+    getline(cin, name);
+
+    for (int i = 0; i < n; ++i)
+    {
+    }
