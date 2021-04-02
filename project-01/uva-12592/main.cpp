c13ec11120389a48e5b6be39125aa355e0a30392
Sarfaroz Yunusov <imSarfaroz@users.noreply.github.com>
2021-04-03 00:20:58 +0500

    project01 uva-12592 incomplete

---
+++ project-01/uva-12592/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    unordered_map<string, string> slogans;
+
+    // read line
+}
