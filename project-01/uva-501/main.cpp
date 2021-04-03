d7d1bb3d1331d0f25f566aa3453d9101ed6abed5
Sarfaroz Yunusov <imSarfaroz@users.noreply.github.com>
2021-04-03 18:37:22 +0500

    project01 uva-501 incomplete logic

---
+++ project-01/uva-501/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <set>
+using namespace std;
+
+int main(void)
+{
+    int n;
+    cin >> n;
+
+    set<int> num;
+    int numbers[1000];
+
+    while (n--)
+    {
+        int a, b;
+        cin >> a >> b;
+
+        for (int i = 0; i < a; i++)
+        {
+            cin >> numbers[i];
+        }
+
+        // insert
+        // calculate
+        // print
+    }
+}
