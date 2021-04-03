8d36b0865b34cf9308aa5ae604226902e932d391
Sarfaroz Yunusov <imSarfaroz@users.noreply.github.com>
2021-04-03 18:58:14 +0500

    project01 uva-13037 unfinished

---
+++ project-01/uva-13037/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int main()
+{
+    int numTest;
+    cin >> numTest;
+
+    for (int i = 0; i < numTest; i++)
+    {
+        set<int> chocolates;
+        int p[3];
+
+        for (int i = 0; i < 3; i++)
+        {
+            cin >> p[i];
+        }
+    }
+}
