e2333b3158afc9fa0f13d0e5566e346154a049e4
Sarfaroz Yunusov <imSarfaroz@users.noreply.github.com>
2021-04-04 13:04:27 +0500

    project01 uva-10145 incomplete logic

---
+++ project-01/uva-10145/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+int main()
+{
+    int numOfTest;
+    cin >> numOfTest;
+
+    string line;
+    getline(cin, line);
+
+    for (int i = 0; i < numOfTest; i++)
+    {
+        getline(cin, line);
+
+        while (getline(cin, line) && line != '#')
+        {
+            char a;
+            int id, owned;
+        }
+    }
+}
