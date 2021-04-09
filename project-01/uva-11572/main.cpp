33371f75a4c7a8d656781ba7496faf4df509451d
Sarfaroz Yunusov <imSarfaroz@users.noreply.github.com>
2021-04-09 14:29:01 +0500

    project01 uva-11572 unfinished

---
+++ project-01/uva-11572/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+int main()
+{
+    int cases;
+    cin >> cases;
+    map<int, int> snows;
+
+    for (int i = 0; i < cases; i++)
+    {
+        int numSnow;
+        cin >> numSnow;
+        int size = 0;
+        int max = 0;
+        int repeated = 0;
+        }
+}
