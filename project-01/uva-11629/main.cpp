c6311f4aae014b38ec7961c3cfbcf94f368549bb
Sarfaroz Yunusov <imSarfaroz@users.noreply.github.com>
2021-04-10 19:54:33 +0500

    project01 uva-11629 unfinished

---
+++ project-01/uva-11629/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+
+int main()
+{
+    int party;
+    int guess;
+    cin >> party;
+    cin >> guess;
+
+    unordered_map<string, int> ballot;
+
+    for (int i = 0; i < party; i++)
+    {
+        string a;
+        cin >> a;
+        float b;
+        cin >> b;
+    }
+}
