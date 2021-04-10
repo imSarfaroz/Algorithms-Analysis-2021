b61061e24a2833534ede5818aaeb6c080331ecc3
Sarfaroz Yunusov <imSarfaroz@users.noreply.github.com>
2021-04-10 20:22:23 +0500

    project01 uva-11629 incomplete logic

--- project-01/uva-11629/main.cpp
+++ project-01/uva-11629/main.cpp
@@ -19,5 +19,31 @@ int main()
         cin >> a;
         float b;
         cin >> b;
+
+        ballot[a] = b;
+    }
+
+    for (int j = 1; j <= guess; j++)
+    {
+        string s;
+        cin >> s;
+        int count = 0;
+
+        while(true)
+        {
+            if(s != "+")
+            {
+                count += ballot[s];
+            }
+            cin >> s;
+        }
+        int n;
+        cin >> n;
+
+        cout << "Case #" << j;
+        if(s == ">")
+        {
+            cout << (count > n ? "was correct." : "was incorrect");
+        }
     }
 }
