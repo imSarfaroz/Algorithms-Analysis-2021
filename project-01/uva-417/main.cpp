fda4a95519ddd8415681704ecd45b5c0c2e6d6a0
Sarfaroz Yunusov <imSarfaroz@users.noreply.github.com>
2021-04-14 21:07:48 +0500

    project01 uva-417 incomplete

--- project-01/uva-417/main.cpp
+++ project-01/uva-417/main.cpp
@@ -15,6 +15,27 @@ int main()
         letters[s] = num++;
     }
 
+    for (int i = 0; i < 26; i++)
+    {
+        for (int j = i + 1; j < 26; j++)
+        {
+            s[0] = 'a' + i, s[1] = 'a' + j;
+            letters[s] = num++;
+        }
+    }
+
+    for (int i = 0; i < 26; i++)
+    {
+        for (int j = i + 1; j < 26; j++)
+        {
+            for (int k = j + 1; k < 26; k++)
+            {
+                s[0] = 'a' + i, s[1] = 'a' + j, s[2] = 'a' + k;
+                letters[s] = num++;
+            }
+        }
+    }
+
     while (cin >> s)
     {
         cout << letters[s] << endl;
