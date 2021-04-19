16e686c70878bef6f8b147028f9e1f14a36c4944
Sarfaroz Yunusov <imSarfaroz@users.noreply.github.com>
2021-04-19 16:36:06 +0500

    project02 uva260 incomplete

--- project-02/uva-260/main.cpp
+++ project-02/uva-260/main.cpp
@@ -5,12 +5,16 @@ using namespace std;
 int main()
 {
     int num;
+    int count = 0;
     char game[100][100];
+    bool been[100][100];
 
     while (cin >> num)
     {
         if (num == 0)
             break;
+
+        count++;
 
         for (int i = 0; i < num; i++)
         {
@@ -18,6 +22,38 @@ int main()
             {
                 cin >> game[i][j];
             }
+        }
+
+        bool hasWon = false;
+
+        // cheking winner
+        for (int i = 0; i < num; i++)
+        {
+            if (game[0][i] == 'b')
+            {
+                for (int a = 0; a < num; i++)
+                {
+                    for (int b = 0; b < num; b++)
+                    {
+                        been[a][b] = false;
+                    }
+                }
+                been[0][i] = true;
+                hasWon = dfs();
+                if (hasWon)
+                    break;
+            }
+        }
+
+        if (hasWon)
+        {
+            cout << count << " "
+                 << "B" << endl;
+        }
+        else
+        {
+            cout << count << " "
+                 << "W" << endl;
         }
     }
 }
