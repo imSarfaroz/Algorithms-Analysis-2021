70cc67c561d550c60156e5b6ffe1bb53b35c6cd0
Sarfaroz Yunusov <imSarfaroz@users.noreply.github.com>
2021-04-19 14:46:10 +0500

    project02 uva-572 dfs func practice

--- project-02/uva-572/main.cpp
+++ project-02/uva-572/main.cpp
@@ -4,9 +4,28 @@
 
 using namespace std;
 
+void dfs(int r, int c, vector<string> &grid)
+{
+    static const vector<int> dr = {-1, -1, 0, 1, 1, 1, 0, -1};
+    static const vector<int> dc = {0, 1, 1, 1, 0, -1, -1, -1};
+
+    grid[r][c] = '|';
+
+    for (int i = 0; i < 8; i++)
+    {
+        int tr = r + dr[i];
+        int tc = c + dc[i];
+
+        if (0 <= tr && tr < int(grid.size()) && 0 <= tc && tc < int(grid[0].size()) && grid[tr][tc] == '@')
+        {
+            dfs(tr, tc, grid);
+        }
+    }
+}
+
 int main()
 {
-    for (int h, w; cin >> h >> w && h != 0 && w != 0)
+    for (int h, w; cin >> h >> w && h != 0 && w != 0;)
     {
         vector<string> grid(h);
 
@@ -23,7 +42,7 @@ int main()
                 if (grid[r][c] == '@')
                 {
                     ++nComps;
-                    dfc(r, c, grid);
+                    dfs(r, c, grid);
                 }
             }
         }
