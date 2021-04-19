c459a66ec146d85587180542d218dd8abd24c64c
Sarfaroz Yunusov <imSarfaroz@users.noreply.github.com>
2021-04-19 14:39:11 +0500

    project02 uva-572 func main()

--- project-02/uva-572/main.cpp
+++ project-02/uva-572/main.cpp
@@ -14,5 +14,19 @@ int main()
         {
             cin >> grid[r];
         }
+
+        int nComps = 0;
+        for (int r = 0; r < h; r++)
+        {
+            for (int c = 0; c < w; ++c)
+            {
+                if (grid[r][c] == '@')
+                {
+                    ++nComps;
+                    dfc(r, c, grid);
+                }
+            }
+        }
+        cout << nComps << "\n";
     }
 }
