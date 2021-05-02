f8e7facd39c94a753e7275bf77bec923a73a8d88
Sarfaroz Yunusov <imSarfaroz@users.noreply.github.com>
2021-05-03 00:02:57 +0500

    project01 uva-10305 incomplete logc

---
+++ project-02/uva-10305/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    int m;
+
+    while (cin >> n >> m)
+    {
+        if (n == 0 && m == 0)
+            break;
+
+        int a;
+        int b;
+
+        for (int i = 1; i <= m; i++)
+        {
+            cin >> a >> b;
+        }
+    }
+}
