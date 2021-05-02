cc186b89654919777a52c470a0e5ac7c8adca5fb
Sarfaroz Yunusov <imSarfaroz@users.noreply.github.com>
2021-05-03 00:14:48 +0500

    project02 uva-10305 incomplete logic

--- project-02/uva-10305/main.cpp
+++ project-02/uva-10305/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std;
+
+int prob[100][100];
+int vis[100];
+
+void dfs(int z)
+{
+}
 
 int main()
 {
@@ -18,6 +27,15 @@ int main()
         for (int i = 1; i <= m; i++)
         {
             cin >> a >> b;
+            prob[a][b] = 1;
+        }
+
+        for (int i = 0; i <= n; i++)
+        {
+            if (vis[i] != 1)
+            {
+                dfs(i);
+            }
         }
     }
 }
