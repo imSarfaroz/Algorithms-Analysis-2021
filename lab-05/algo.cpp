c9d05872a0acfd7b43bb82e7f0f16fe3d599a079
Sarfaroz Yunusov <imSarfaroz@users.noreply.github.com>
2021-05-03 12:51:53 +0500

    lab-05 insertionSort()

--- lab-05/algo.cpp
+++ lab-05/algo.cpp
@@ -4,3 +4,18 @@
 #include <iostream>
 
 using namespace std;
+
+void insertSort(vector<int> &v)
+{
+    for (int i = 1; i < v.size(); i++)
+    {
+        int j = i;
+        while (j > 0 && v[j] < v[j - 1])
+        {
+            int temp = v[j - 1];
+            v[j - 1] = v[j];
+            v[j] = temp;
+            j = j - 1;
+        }
+    }
+}
