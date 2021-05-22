a292f3bb66b09ffbd4db0e0a74af539040ed4217
imsarfaroz2 <yunusov_s@auca.kg>
2021-05-22 20:44:31 +0500

    project02 uva-429 completed

--- project-02/uva-429/main.cpp
+++ project-02/uva-429/main.cpp
@@ -22,19 +22,22 @@ int main()
             words.push_back(word);
         }
 
+        cin.ignore();
+
         string line;
-        while (getline(cin, line))
+        while (getline(cin, line) && line != "")
         {
-            stringstream ss(line);
+            stringstream ss(line);  
             string w1, w2;
             ss >> w1 >> w2;
 
             // BFS
             queue<string> q;
             map<string, int> simWords;
+            simWords[w1] = 0;
             q.push(w1);
 
-            while (!q.empty())
+            while (!q.empty() && simWords.count(w2) == 0)
             {
                 string u = q.front();
                 q.pop();
@@ -45,15 +48,22 @@ int main()
                     if (!simWords.count(v) && u.length() == v.length())
                     {
                         int diff = 0;
+
                         for (size_t j = 0; j < u.length(); ++j)
-                            if (diff == 1)
-                            {
-                                // c
-                            }
+                            if (u[j] != v[j])
+                                ++diff;
+
+                        if (diff == 1)
+                        {
+                            simWords[v] = simWords[u] + 1;
+                            q.push(v);
+                        }
                     }
                 }
             }
-            cout << w1 << " " << w2 << endl;
+            cout << w1 << " " << w2 << " " << simWords[w2] << endl;
         }
+        if (num)
+            cout << endl;
     }
 }
