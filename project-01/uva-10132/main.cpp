230c2d6ac4b0b1fb499d9ed4118e227507f6824a
imsarfaroz2 <yunusov_s@auca.kg>
2021-05-23 02:49:08 +0500

    project01 uva-10132 done

--- project-01/uva-10132/main.cpp
+++ project-01/uva-10132/main.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 #include <sstream>
+#include <map>
 
 using namespace std;
 
@@ -26,5 +27,24 @@ int main()
         }
 
         // concetations
+        map<string, int> memo;
+        for (size_t i = 0; i < fragments.size(); ++i)
+            for (size_t j = i + 1; j < fragments.size(); ++j)
+            {
+                ++memo[fragments[i] + fragments[j]];
+                ++memo[fragments[j] + fragments[i]];
+            }
+
+        // Search for the string of highest count.
+        map<string, int>::iterator iter(memo.begin());
+        map<string, int>::iterator file(memo.begin());
+        for (; iter != memo.end(); ++iter)
+        {
+            if (iter->second > file->second)
+                file = iter;
+        }
+        cout << file->first << endl;
+        if (n)
+            cout << endl;
     }
 }
